f999cfd59ba419a42d7d659bbd30417df6182976
hclsonu <sonu-yadav@hcltech.com>
2024-08-23 17:59:26 +0530

    Task: Add header for good style

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import './App.css';
-import SearchBar from './components/SearchBar';
 import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+
 
 function App() {
   return (
+    <>
+    <Header />
     <Container>
       <h1 className="my-4">Movie Search Engine</h1>
       <SearchBar />
     </Container>
+    </>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navbar, Nav, Container, Form, FormControl, Button } from 'react-bootstrap';
+
+const Header: React.FC = () => {
+    return (
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">My Movie App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbar-nav" />
+        </Container>
+      </Navbar>
+    );
+  };
+  
+  export default Header;
